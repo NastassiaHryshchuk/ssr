83cc56a9c0f5933189d7710a23d598454caa487b
Nastassia Hryshchuk <19anastasia86@gmail.com>
2018-06-17 23:27:02 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const app = require('./app');
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.info(`Express listening on port ${port}`); // eslint-disable-line
+});const app = require('./app');
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.info(`Express listening on port ${port}`); // eslint-disable-line
+});
